1565f703128aa221511c7348941906fcaa006b16
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2017-08-31 21:11:04 +0200

    Data loading in separate module.

---
+++ app/datawrangler.py
@@ -0,0 +1,40 @@
+"""Handles data readin and preprocessing."""
+import pandas as pd
+
+
+def pkg_profit(pkg_price, pkg_hours, wage):
+    return pkg_price - pkg_hours * wage
+
+
+def load_timetracking_data(path):
+    """Loading the data"""
+    tt_data = pd.read_csv(path, parse_dates=[0])
+    # There are about 3,5K (~10% of overall data) entries which we can't use
+    # Also there are 1364 entries where the complexity field is null
+    tt_data_filter = ((tt_data['Complexity'] != 'NotSpecified') &
+                      (tt_data['Complexity'] != 'Special') & (~(tt_data['Complexity'].isnull())))
+    tt_data = tt_data[tt_data_filter]
+    # For now drop duplicates. Would be nice to investigate where these are coming from
+    tt_data = tt_data.drop_duplicates()
+    # sort everything by date
+    tt_data = tt_data.sort_values('Date')
+    # To make life easier later, store column name for total packaging time as a constant
+    TOTAL_TIME = 'Total Time (1)'
+    # Only use subset of columns
+    tt_data = tt_data[['Date', 'Project', 'Package Number', 'Complexity', TOTAL_TIME]]
+    tt_data = tt_data.rename(columns={'Date': 'x', 'Package Number': 'Pkg_ID'})
+
+    # This is all static for now until we get access to the actual data
+    # The prices came from Mathias
+    package_prices = {"Basic": 100, "Easy": 180, "Medium": 360, "Complex": 680}
+    tt_data['Price'] = tt_data['Complexity'].apply(lambda cpl: package_prices[cpl])
+    # Also static for now, pending us figuring something nicer out later
+    HOURLY_WAGE = 18
+
+    tt_data['y'] = pkg_profit(tt_data["Price"], tt_data[TOTAL_TIME], HOURLY_WAGE)
+
+    planned_times = {"Basic": 2, "Easy": 4, "Medium": 8, "Complex": 16}
+    tt_data['planned_y'] = pkg_profit(tt_data['Price'],
+                                      tt_data['Complexity'].apply(lambda cpl: planned_times[cpl]),
+                                      HOURLY_WAGE)
+    return tt_data
--- app/main.py
+++ app/main.py
@@ -1,51 +1,13 @@
 from collections import namedtuple
 
-import pandas as pd
-
 from bokeh.layouts import layout, widgetbox
 from bokeh.models.widgets import Slider, Select
 from bokeh.io import curdoc
 
 from ttplotter import generate_plot, update
+from datawrangler import load_timetracking_data
 
-# Loading the data
-# TODO: where do we get data from?
-tt_data = pd.read_csv('/data/time_tracking.csv', parse_dates=[0])
-# There are about 3,5K (~10% of overall data) entries which we can't use
-# Also there are 1364 entries where the complexity field is null
-tt_data_filter = ((tt_data['Complexity'] != 'NotSpecified')
-                  & (tt_data['Complexity'] != 'Special')
-                  & (~(tt_data['Complexity'].isnull())))
-tt_data = tt_data[tt_data_filter]
-# For now drop duplicates. Would be nice to investigate where these are coming from
-tt_data = tt_data.drop_duplicates()
-# sort everything by date
-tt_data = tt_data.sort_values('Date')
-# To make life easier later, store column name for total packaging time as a constant
-TOTAL_TIME = 'Total Time (1)'
-# Only use subset of columns
-tt_data = tt_data[['Date', 'Project', 'Package Number', 'Complexity', TOTAL_TIME]]
-tt_data = tt_data.rename(columns={'Date': 'x', 'Package Number': 'Pkg_ID'})
-
-# This is all static for now until we get access to the actual data
-# The prices came from Mathias
-package_prices = {"Basic": 100, "Easy": 180, "Medium": 360, "Complex": 680}
-tt_data['Price'] = tt_data['Complexity'].apply(lambda cpl: package_prices[cpl])
-# Also static for now, pending us figuring something nicer out later
-HOURLY_WAGE = 18
-
-
-def pkg_profit(pkg_price, pkg_hours, wage):
-    return pkg_price - pkg_hours * wage
-
-
-tt_data['y'] = pkg_profit(tt_data["Price"], tt_data[TOTAL_TIME], HOURLY_WAGE)
-
-planned_times = {"Basic": 2, "Easy": 4, "Medium": 8, "Complex": 16}
-tt_data['planned_y'] = pkg_profit(tt_data['Price'],
-                                  tt_data['Complexity'].apply(lambda cpl: planned_times[cpl]),
-                                  HOURLY_WAGE)
-
+tt_data = load_timetracking_data('/data/time_tracking.csv')
 default_data = tt_data[tt_data['Complexity'] == "Basic"]
 p = generate_plot(default_data)
 
